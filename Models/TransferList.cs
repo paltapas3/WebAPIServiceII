10678cbcf7ca0b01e77422a6437d1488d30a2c9d
Tapas Pal <paltapas3@gmail.com>
2018-08-24 15:52:33 +0530

    Added New Service

---
+++ Models/TransferList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPIServiceII.Models
+{
+    public static class TransferList
+    {
+        public static List<AmountTransfer> _amtList = new List<AmountTransfer>();
+    }
+}
